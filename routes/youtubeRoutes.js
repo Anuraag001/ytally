df22ac491993126e1546ba4a0ed3a24ee638fc17
Anuraag001 <anuraagbv1@gmail.com>
2024-02-11 02:50:53 +0530

    universal yt post request made

--- routes/youtubeRoutes.js
+++ routes/youtubeRoutes.js
@@ -2,22 +2,27 @@ const express = require('express');
 const Route = express.Router();
 const axios = require('axios');
 
-Route.get('/all', (req, res) => {
-  const apiKey = 'AIzaSyDhngiHaM5o7q_2iQRJgHhGsSqnvtctoLc'; // Replace with your actual API key
-  const query = 'codebasics'; // Replace with your search query
-
-  // Construct the URL for YouTube Data API search
-  const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=${apiKey}`;
-
-  // Make the request using axios
-  axios.get(apiUrl)
-    .then(response => {
-      res.json(response.data);
-    })
-    .catch(error => {
-      console.error('Error performing search:', error.message);
-      res.status(500).json({ message: 'Internal server error' });
+Route.post('/all', (req, res) => {
+    const apiKey = 'AIzaSyDhngiHaM5o7q_2iQRJgHhGsSqnvtctoLc';
+  
+    const queryParams = req.query;
+  
+    const params = new URLSearchParams();
+    Object.entries(queryParams).forEach(([key, value]) => {
+      params.append(key, value);
     });
-});
+  
+    params.append('key', apiKey);
+    const apiUrl = `https://www.googleapis.com/youtube/v3/search?${params.toString()}`;
+  
+    axios.get(apiUrl)
+      .then(response => {
+        res.json(response.data);
+      })
+      .catch(error => {
+        console.error('Error performing search:', error.message);
+        res.status(500).json({ message: 'Internal server error' });
+      });
+  });
 
 module.exports = Route;
